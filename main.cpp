04e7a40ecf6c73b70ce08ebad53cb6fe6438b686
maggie-bao202 <maggie.bao202@gmail.com>
2020-05-27 22:15:40 -0700

    test

--- main.cpp
+++ main.cpp
@@ -11,6 +11,19 @@ struct Node{
   vector<int> weights;
 };
 
+int isConnected(Node* from, Node* find){
+   vector<Node*> :: iterator it;
+   int count = 0;
+   for(it = from->connect.begin(); it != from->connect.end(); it++){
+     if ((*it) == find){
+       return count;
+     }
+     count++;
+   }
+   return -1;
+}
+
+
 void addVertex(vector<Node*> vlist){
   Node* temp = new Node();
   temp->label = new char(50);
@@ -25,6 +38,8 @@ bool addEdge(vector<Node*> vlist){
   bool yone = false;
   bool ytwo = false;
   int weight = 0;
+  Node* one = NULL;
+  Node* two = NULL;
   vector<Node*> :: iterator it;
   cout << "Vertex 1: " << endl;
   cin.getline(ione, 50);
@@ -32,15 +47,15 @@ bool addEdge(vector<Node*> vlist){
   cin.getline(itwo, 50);
    for(it = vlist.begin(); it != vlist.end(); it++){
      if (strcmp((*it)->label, ione) == 0){
-       Node* one = *(it);
+       one = *(it);
        yone = true;
      }
      if (strcmp((*it)->label, itwo) == 0){
-       Node* two = *(it);
+       two = *(it);
        ytwo = true;
      }
    }
-   if (yone = true && ytwo = true){
+   if (yone == true && ytwo == true){
      cout << "Edge Weight: " << endl;
      cin >> weight;
      cin.ignore();
@@ -54,31 +69,42 @@ bool addEdge(vector<Node*> vlist){
 void removeVertex(vector<Node*> vlist){
   char* input = new char(50);
   bool found = false;
+  int i = 0;
+  Node* temp = NULL;
   vector<Node*> :: iterator it;
   cout << "Vertex Label: " << endl;
   cin.getline(input, 50);
   do{
   for(it = vlist.begin(); it != vlist.end(); it++){
     if (((*it)->label, input) == 0) {
-      Node* temp = *(it);
+      temp = *(it);
       found = true;
     }
   }
   } while (found == false);
   for(it = vlist.begin(); it != vlist.end(); it++){
-    if ((*it)->connect == find) {
+    i = isConnected((*it), temp);
+    if (i != -1){
+      (*it)->connect.erase((*it)->connect.begin() + i);
+      (*it)->weights.erase((*it)->weights.begin() + i);
+    }
+  }
+  for(it = vlist.begin(); it != vlist.end(); it++){
+    if ((*it) == temp){
+      vlist.erase(it);
+      delete temp;
     }
   }
 }
-  
-    
 
 void removeEdge(vector<Node*> vlist){
   char* ione = new char(50);
   char* itwo = new char(50);
   bool yone = false;
   bool ytwo = false;
-  int weight = 0;
+  Node* one = NULL;
+  Node* two = NULL;
+  int i = 0;
   vector<Node*> :: iterator it;
   cout << "Vertex 1: " << endl;
   cin.getline(ione, 50);
@@ -86,18 +112,24 @@ void removeEdge(vector<Node*> vlist){
   cin.getline(itwo, 50);
   for(it = vlist.begin(); it != vlist.end(); it++){
      if (strcmp((*it)->label, ione) == 0){
-       Node* one = *(it);
+       one = *(it);
        yone = true;
      }
      if (strcmp((*it)->label, itwo) == 0){
-       Node* two = *(it);
+       two = *(it);
        ytwo = true;
      }
   }
-  if (yone = true && ytwo = true){
-    one->connect.erase(one->connect.begin() + index);
-    one->weights.erase(one->weights.begin() + index);
-
+  if (yone == true && ytwo == true){
+    i = isConnected(one, two);
+    if (i != -1){
+      one->connect.erase(one->connect.begin() + i);
+      one->weights.erase(one->weights.begin() + i);
+      return;
+    }
+    cout << "Nodes Not Connected" << endl;
+  }
+}
 
 
 int main(){
