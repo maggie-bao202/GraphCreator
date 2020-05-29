591d95a85626509dba92d3d654bbb4b3ec09c342
maggie-bao202 <maggie.bao202@gmail.com>
2020-05-28 18:25:50 -0700

    shortest path

--- main.cpp
+++ main.cpp
@@ -2,10 +2,8 @@
 #include <cstring>
 #include <vector>
 #include <fstream>
-#include "stdlib.h"
-#include "assert.h"
 #include <iterator>
-
+#include <string.h>
 using namespace std;
 
 struct Node{
@@ -14,12 +12,12 @@ struct Node{
   vector<int> weights;
 };
 
-int isConnected(Node* from, Node* find);
+int distance(Node* x, Node* y);
 void addVertex(vector<Node*>* vlist);
 void addEdge(vector<Node*>* vlist);
 void removeVertex(vector<Node*>* vlist);
 void removeEdge(vector<Node*>* vlist);
-
+void printTable(vector<Node*> vlist);
 int main(){
   vector<Node*> vlist;
   char* keyword = new char[20];
@@ -38,12 +36,41 @@ int main(){
     }
     else if (strcmp(keyword, "RV") == 0){
       removeVertex(&vlist);
+      /*char* input = new char[20];
+      Node* temp = NULL;
+      cout << "Vertex Label: " << endl;
+      cin.getline(input, 50);
+      vector<Node*> :: iterator it;
+      for (it = vlist.begin(); it != vlist.end(); it++){
+	if (strcmp((*it)->label, input) == 0){
+	  temp = *(it);
+	}
+      }
+      if (temp == NULL){
+	cout << "Invalid Node!" << endl;
+	continue;
+      }
+      vector<Node*> :: iterator ite;
+      for (ite = vlist.begin(); ite != vlist.end(); ite++){
+	int i = isConnected((*ite), temp);
+	if (i != -1){//disconnect
+	  (*ite)->connect.erase((*ite)->connect.begin() + i);
+	  (*ite)->weights.erase((*ite)->weights.begin() + i);
+	}
+      }
+      for(int j = 0; j < vlist.size(); j++){
+	if (vlist[j] == temp){
+	  vlist.erase(vlist.begin() + j);
+	  delete temp;
+	}
+      }
+      printTable(vlist);*/
     }
     else if (strcmp(keyword, "RE") == 0){
       removeEdge(&vlist);
     }
-    else if (strcmp(keyword, "PRINT")){
-      
+    else if (strcmp(keyword, "PRINT") == 0){
+      printTable(vlist);
     }
     else if (strcmp(keyword, "SP") == 0){
     }
@@ -60,14 +87,12 @@ int main(){
   return 0;
 }
 
-int isConnected(Node* from, Node* find){
-   vector<Node*> :: iterator it;
-   int count = 0;
-   for(it = from->connect.begin(); it != from->connect.end(); it++){
-     if ((*it) == find){
-       return count;
+int distance(Node* x, Node* y){
+  vector<Node*> :: iterator it;
+  for(it = x->connect.begin(); it != x->connect.end(); it++){
+     if ((*it) == y){
+       return it - x->connect.begin();
      }
-     count++;
    }
    return -1;
 }
@@ -89,10 +114,10 @@ void addEdge(vector<Node*>* vlist){
   int weight = 0;
   Node* one = NULL;
   Node* two = NULL;
-  /*if (vlist->size() < 2 ){
+  if (vlist->size() < 2 ){
     cout << "Not Enough Nodes!" << endl;
     return;
-    }*/
+  }
   vector<Node*> :: iterator it;
   cout << "Vertex 1: " << endl;
   cin.getline(ione, 50);
@@ -100,7 +125,8 @@ void addEdge(vector<Node*>* vlist){
   cin.getline(itwo, 50);
    for(it = vlist->begin(); it != vlist->end(); it++){
      if (strcmp((*it)->label, ione) == 0){
-       one = *(it);
+       cout << (*it)->label<<endl;
+       one = (*it);
        yone = true;
      }
      if (strcmp((*it)->label, itwo) == 0){
@@ -120,48 +146,45 @@ void addEdge(vector<Node*>* vlist){
 }
 
 void removeVertex(vector<Node*>* vlist){
-  char* input = new char(50);
-  bool found = false;
-  int i = 0;
+  char* input = new char[20];
   Node* temp = NULL;
+  cout << "Vertex Label: " << endl;
+  cin.getline(input, 50);
   vector<Node*> :: iterator it;
-  do{
-    cout << "Vertex Label: " << endl;
-    cin.getline(input, 50);
-    for(it = vlist->begin(); it != vlist->end(); it++){
-      if (strcmp((*it)->label, input) == 0) {
-	temp = *(it);
-	found = true;
-      }
-    }
-    if (found == false){
-      cout << "Vertex Not Found!" << endl;
-    }
-  } while (found == false);
-  cout << "d" << endl;
-  for(it = vlist->begin(); it != vlist->end(); it++){
-    cout <<"d" << endl;
-    i = isConnected((*it), temp);
-    if (i != -1){
-      (*it)->connect.erase((*it)->connect.begin() + i);
-      (*it)->weights.erase((*it)->weights.begin() + i);
+  for (it = vlist->begin(); it != vlist->end(); it++){
+    if (strcmp((*it)->label, input) == 0){
+      temp = *(it);
     }
   }
-  for(it = vlist->begin(); it != vlist->end(); it++){
-    if ((*it) == temp){
-      vlist->erase(it);
+  if (temp == NULL){
+    cout << "Invalid Node!" << endl;
+    return;
+  }
+  vector<Node*> :: iterator ite;
+  for (ite = vlist->begin(); ite != vlist->end(); ite++){
+    int i = distance((*ite), temp);
+    if (i != -1){//disconnect
+      (*ite)->connect.erase((*ite)->connect.begin() + i);
+      (*ite)->weights.erase((*ite)->weights.begin() + i);
+    }
+  }
+  vector<Node*> :: iterator iter;
+  for(iter = vlist->begin(); iter != vlist->end(); iter++){
+    if (temp == (*iter)){
+      vlist->erase(iter);
+      iter--;
       delete temp;
     }
   }
 }
- 
+
 void removeEdge(vector<Node*>* vlist){
   char* ione = new char(50);
   char* itwo = new char(50);
-  bool yone = false;
-  bool ytwo = false;
   Node* one = NULL;
   Node* two = NULL;
+  bool yone = false;
+  bool ytwo = false;
   int i = 0;
   vector<Node*> :: iterator it;
   cout << "Vertex 1: " << endl;
@@ -169,22 +192,51 @@ void removeEdge(vector<Node*>* vlist){
   cout << "Vertex 2: " << endl;
   cin.getline(itwo, 50);
   for(it = vlist->begin(); it != vlist->end(); it++){
-     if (strcmp((*it)->label, ione) == 0){
-       one = *(it);
-       yone = true;
-     }
-     if (strcmp((*it)->label, itwo) == 0){
-       two = *(it);
-       ytwo = true;
-     }
+    if (strcmp((*it)->label, ione) == 0){
+      one = *(it);
+      yone = true;
+    }
+    if (strcmp((*it)->label, itwo) == 0){
+      two = *(it);
+      ytwo = true;
+    }
   }
   if (yone == true && ytwo == true){
-    i = isConnected(one, two);
+    i = distance(one, two);
     if (i != -1){
       one->connect.erase(one->connect.begin() + i);
       one->weights.erase(one->weights.begin() + i);
       return;
     }
-    cout << "Nodes Not Connected" << endl;
+  }
+  cout << "Invalid Vertices!" << endl;
+}
+
+void printTable(vector<Node*> vlist){
+  vector<Node*> :: iterator it;
+  vector<Node*> :: iterator i;
+  vector<Node*> :: iterator j;
+  cout << "Adjacency Table:" << endl << endl;
+  cout << "\t";
+  for (it = vlist.begin(); it != vlist.end(); it++){
+    cout << (*it)->label;
+    cout << "\t";
+  }
+  cout << endl;
+  for (i = vlist.begin(); i!= vlist.end(); i++){
+    cout << (*i)->label;
+    cout << "\t";
+    for (j = vlist.begin(); j != vlist.end(); j++){
+      if (distance((*i), (*j)) != -1) {
+	cout << (*i)->weights[distance((*i),(*j))];
+	cout << "\t";
+      }
+      else{
+	cout << "*";
+	cout << "\t";
+      }
+    }
+    cout << endl;
   }
 }
+     
